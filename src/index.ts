0136803fc4c469567299d1b64be9ca101dd20c06
jake.liu <jake.liu@baozun.com>
2022-05-05 16:22:22 +0800

    typescript 函数

--- src/index.ts
+++ src/index.ts
@@ -26,3 +26,103 @@ enum Database {
     REDIS = 'redis',
 }
 const db1 = Database.MYSQL
+
+// ----------Union -------------
+let bbb: number | string 
+bbb=111
+bbb='联合'
+
+
+// -----------Type --------------
+type A= number | string 
+let a1:A
+a1 = 999
+a1 = 'typescript'
+
+// -----------interface --------------
+interface User {
+    name: string;
+    age: number;
+}
+
+// ----------- object --------------
+type Card ={
+    name: string
+    desc: string 
+}
+
+interface Card2 {
+    name: string;
+    desc: string;
+}
+
+interface Card2{
+    age?: number // ?表示可选
+}
+
+// 定义为type 类型
+const obj: Card = {
+    name:"tom",
+    desc:'测试type类型'
+}
+
+// 定义为interface
+const obj2: Card2 = {
+    name:"tom",
+    desc:'测试type类型',
+    age:1
+}
+
+// ------------function -------------
+// 参数
+// 函数会自动推倒出函数的返回值
+function hello(a:string,b:string){
+    return a+b
+}
+
+// 返回number
+function hello2(a:string,b:string): number{
+    console.log(a,b);
+    return 9999;
+}
+
+function hello3(a:string,b:string,c:string){
+    return 100;
+}
+
+// undefined
+function hello4(name:string,age?:number){
+    return age
+}
+
+const func =()=>{}
+const func2 =()=>{
+    return 1
+}
+
+// ----------- unknown --------------
+type Data ={
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean
+}
+async function getData() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const data = await res.json() as Data;
+    console.log(data);
+}
+
+const data1: Data={
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+}
+
+type Beta ={
+    name:string 
+}
+
+// 
+const beta = data1 as unknown as Beta
